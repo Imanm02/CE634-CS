6b9e4017cedc7793b270dcdb04d3fd4bc20134d6
Imanm02 <imanm1381@gmail.com>
2023-12-10 22:15:40 +0330

    Update HW2 Question 6 code

--- Assignments/Assignment 2/main.py
+++ Assignments/Assignment 2/main.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def calculate_state_probability(initial_probabilities, transition_matrix, N):
+def calculate_state_probability(p0, transition_matrix, N, s):
     """
-    محاسبه احتمال حالت در روز ورودی با استفاده از بردار احتمال اولیه و ماتریس انتقال
+    محاسبه احتمال استیت s در روز Nام با استفاده از بردار احتمال اولیه p0 و ماتریس انتقال
     """
-    current_probabilities = np.array(initial_probabilities)
+    current_probabilities = np.array(p0)
     for _ in range(N):
         current_probabilities = np.dot(current_probabilities, transition_matrix)
-    return current_probabilities
+    return current_probabilities[s]
 
 # تعریف بردار احتمال اولیه و ماتریس انتقال
 initial_probabilities = [0.3, 0.2, 0.3, 0.2]
@@ -19,19 +19,23 @@ transition_matrix = [
 ]
 
 # محاسبه احتمال انتخاب جوجه کباب در روز 777ام
-probabilities_day_777 = calculate_state_probability(initial_probabilities, transition_matrix, 777)
+probabilities_day_777 = calculate_state_probability(initial_probabilities, transition_matrix, 777, 1)
 probability_chicken_kebab_day_777 = probabilities_day_777[1] # جوجه کباب دومین غذا است
 
 import matplotlib.pyplot as plt
 
 days = range(10, 10001, 10) # روزهای ۱۰ تا ۱۰۰۰۰
-probabilities = [calculate_state_probability(initial_probabilities, transition_matrix, day)[1] for day in days]
 
-plt.plot(days, probabilities)
+# احتمالات انتخاب جوجه کباب در روزهای مختلف
+probabilities_chicken_kebab = [calculate_state_probability(initial_probabilities, transition_matrix, day, 1) for day in days]
+
+# نمایش نمودار
+plt.plot(days, probabilities_chicken_kebab)
 plt.xlabel('روز')
 plt.ylabel('احتمال انتخاب جوجه کباب')
 plt.title('احتمال انتخاب جوجه کباب در روزهای مختلف')
 plt.show()
+
 
 def simulate_markov_chain(transition_matrix, N, num_simulations):
     """
