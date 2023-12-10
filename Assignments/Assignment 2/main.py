8deca3f3543f59512a7d11950e44af33ab691567
Imanm02 <imanm1381@gmail.com>
2023-12-10 22:07:37 +0330

    Update HW2 Question 6 code part B

--- Assignments/Assignment 2/main.py
+++ Assignments/Assignment 2/main.py
@@ -9,3 +9,15 @@ def calculate_state_probability(initial_probabilities, transition_matrix, N):
         current_probabilities = np.dot(current_probabilities, transition_matrix)
     return current_probabilities
 
+# تعریف بردار احتمال اولیه و ماتریس انتقال
+initial_probabilities = [0.3, 0.2, 0.3, 0.2]
+transition_matrix = [
+    [0.3, 0.5, 0.1, 0.1],
+    [0.5, 0.3, 0.1, 0.1],
+    [0.2, 0.2, 0.2, 0.4],
+    [0.4, 0.4, 0.2, 0]
+]
+
+# محاسبه احتمال انتخاب جوجه کباب در روز 777ام
+probabilities_day_777 = calculate_state_probability(initial_probabilities, transition_matrix, 777)
+probability_chicken_kebab_day_777 = probabilities_day_777[1] # جوجه کباب دومین غذا است
