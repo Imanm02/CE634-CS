52caf96c8cceab81e99d007475d4b6ffc8599ac1
Imanm02 <imanm1381@gmail.com>
2024-01-03 11:44:19 +0330

    Update Q3 code

--- Assignments/Assignment 3/Q3.py
+++ Assignments/Assignment 3/Q3.py
@@ -1,22 +1,29 @@
 from scipy.special import factorial
 
-lambda_ = 1
-mu = 4
-c = 2
+lambda_ = 1  # نرخ ورود بیماران (بیمار در ساعت)
+mu = 4       # نرخ خدمات پزشکان (بیمار در ساعت)
+c = 2        # تعداد پزشکان
 
+# محاسبه فاکتور استفاده از پزشکان
 rho = lambda_ / (c * mu)
 
+# محاسبه احتمال عدم وجود بیمار در سیستم
 p0 = (sum([(c * rho)**n / factorial(n) for n in range(c)]) + 
       ((c * rho)**c / factorial(c)) * (1 / (1 - rho))) ** -1
 
+# محاسبه میانگین تعداد بیماران در سیستم
 l = (rho * (c * rho)**c / (factorial(c) * (1 - rho)**2)) * p0 + c * rho
 
+# محاسبه میانگین تعداد بیماران در صف
 lq = (rho * (c * rho)**c / (factorial(c) * (1 - rho)**2)) * p0
 
+# محاسبه میانگین زمان انتظار در سیستم
 w = l / lambda_
 
+# محاسبه میانگین زمان انتظار در صف
 wq = lq / lambda_
 
+# محاسبه درصد زمان بیکاری پزشکان
 idle_time_percentage = (1 - rho) * 100
 
 print(l, w * 60, idle_time_percentage)
