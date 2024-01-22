1b88f21bde8d1ca80a170e17d3bfd1824c5beb98
Imanm02 <imanm1381@gmail.com>
2024-01-22 22:38:19 +0330

    Update Assignment 4 Question 4 Solution

--- Assignments/Assignment 4/Q4.py
+++ Assignments/Assignment 4/Q4.py
@@ -1,4 +1,23 @@
+import pandas as pd
 import numpy as np
+
+# داده‌های جدول
+data = {
+    "Interval": ["15-30", "30-45", "45-60", "60-90", "90-120", "120-180", "180-300"],
+    "Frequency": [10, 20, 25, 35, 30, 20, 10]
+}
+
+# تبدیل داده‌ها به DataFrame
+df = pd.DataFrame(data)
+
+# محاسبه فراوانی کل
+total_frequency = df['Frequency'].sum()
+
+# محاسبه فراوانی نسبی
+df['Relative Frequency'] = df['Frequency'] / total_frequency
+
+# محاسبه توزیع تجمعی
+df['Cumulative Distribution'] = df['Relative Frequency'].cumsum()
 
 # تولید 5 عدد تصادفی R_i در بازه [0, 1]
 random_numbers = np.random.uniform(0, 1, 5)
